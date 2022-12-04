eeff8a99d4f928fc7f8c01772bceeb2df7ec2b23
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-04 22:00:01 +0900

    :sparkles: :: Add post entity

---
+++ src/main/kotlin/com/example/daangnclone/domain/post/data/entity/Post.kt
@@ -0,0 +1,19 @@
+package com.example.daangnclone.domain.post.data.entity
+
+import com.example.daangnclone.global.entity.BaseTimeEntity
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Post(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // @Id 기본키 @GeneratedValue 기본키 생성 조건 IDENTITY 생성될 때 마다 1씩 증가
+    val idx: Long,
+    val title: String,
+    val content: String,
+    val price: Long,
+    val isSuggestion: Boolean,
+
+) : BaseTimeEntity() {
+}
