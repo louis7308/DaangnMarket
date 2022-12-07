cf4741c3ec707e9511247cad4da0aa1df3d10809
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-07 20:59:10 +0900

    :sparkles: :: 전체 조회 페이지네이션 함수 영속성
    - JPA Custom 함수 제작 기능으로 페이지네이션 기능 제작

---
+++ src/main/kotlin/com/example/daangnclone/domain/post/repository/PostRepository.kt
@@ -0,0 +1,10 @@
+package com.example.daangnclone.domain.post.repository
+
+import com.example.daangnclone.domain.post.data.entity.Post
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PostRepository : JpaRepository<Post, Long> {
+    fun findBy(pageable : Pageable) : Page<Post>
+}
