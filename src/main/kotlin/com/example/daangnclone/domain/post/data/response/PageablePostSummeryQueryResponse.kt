f162c08c23cb71cc3c35d80ad9c9cd1603c5eeb4
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-08 11:02:10 +0900

    :sparkles: :: PageablePostSummeryQueryResponse
    - Service 에서 return 받은 값을 Controller 에 return 값 매칭 시켜주는 Converter

---
+++ src/main/kotlin/com/example/daangnclone/domain/post/data/response/PageablePostSummeryQueryResponse.kt
@@ -0,0 +1,7 @@
+package com.example.daangnclone.domain.post.data.response
+
+import org.springframework.data.domain.Page
+
+class PageablePostSummeryQueryResponse(
+    val data: Page<PostSummeryQueryResponse>
+)
