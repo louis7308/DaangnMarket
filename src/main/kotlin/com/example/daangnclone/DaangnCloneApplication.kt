a6f0b6165b93151b7f0e1e93343baba003bcb418
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-09 23:14:55 +0900

    :sparkles: :: EnableRedis 세팅

--- src/main/kotlin/com/example/daangnclone/DaangnCloneApplication.kt
+++ src/main/kotlin/com/example/daangnclone/DaangnCloneApplication.kt
@@ -1,11 +1,14 @@
 package com.example.daangnclone
 
+import com.example.daangnclone.domain.post.data.entity.Post
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisRepositories
 class DaangnCloneApplication
 
 fun main(args: Array<String>) {
