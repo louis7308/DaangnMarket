a8e27e9c8bb0afb83b7b5aacfd9efc5ac235615a
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-12 15:35:49 +0900

    :sparkles: :: toResponse Converter 메소드 정의

--- src/main/kotlin/com/example/daangnclone/domain/post/util/PostQueryConverter.kt
+++ src/main/kotlin/com/example/daangnclone/domain/post/util/PostQueryConverter.kt
@@ -3,10 +3,13 @@ package com.example.daangnclone.domain.post.util
 import com.example.daangnclone.domain.post.data.dto.PostDto
 import com.example.daangnclone.domain.post.data.dto.PostQueryDto
 import com.example.daangnclone.domain.post.data.response.PageablePostSummeryQueryResponse
+import com.example.daangnclone.domain.post.data.response.PostQueryResponse
 import com.example.daangnclone.domain.post.data.response.PostSummeryQueryResponse
 
 interface PostQueryConverter {
     fun toQueryDto(views: Int, dto: PostDto) : PostQueryDto
     fun toSummaryResponse(dto: PostQueryDto): PostSummeryQueryResponse
     fun toPageableResponse(list: List<PostSummeryQueryResponse>): PageablePostSummeryQueryResponse
+    fun toResponse(dto: PostQueryDto): PostQueryResponse
+
 }
