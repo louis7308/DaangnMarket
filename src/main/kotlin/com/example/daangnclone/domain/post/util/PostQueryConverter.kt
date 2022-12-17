fee0e292478576c89497807556d33c6fd946be5d
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-17 23:49:40 +0900

    :sparkles: :: toEvent Converter

--- src/main/kotlin/com/example/daangnclone/domain/post/util/PostQueryConverter.kt
+++ src/main/kotlin/com/example/daangnclone/domain/post/util/PostQueryConverter.kt
@@ -2,6 +2,7 @@ package com.example.daangnclone.domain.post.util
 
 import com.example.daangnclone.domain.post.data.dto.PostDto
 import com.example.daangnclone.domain.post.data.dto.PostQueryDto
+import com.example.daangnclone.domain.post.data.event.PostQueryEvent
 import com.example.daangnclone.domain.post.data.response.PageablePostSummeryQueryResponse
 import com.example.daangnclone.domain.post.data.response.PostQueryResponse
 import com.example.daangnclone.domain.post.data.response.PostSummeryQueryResponse
@@ -12,4 +13,5 @@ interface PostQueryConverter {
     fun toPageableResponse(list: List<PostSummeryQueryResponse>): PageablePostSummeryQueryResponse
     fun toResponse(dto: PostQueryDto): PostQueryResponse
 
+    fun toEvent(dto: PostQueryDto): PostQueryEvent
 }
